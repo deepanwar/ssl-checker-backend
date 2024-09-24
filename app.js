9fead9da2d8c7ea7d65d4886902e4569a9683e91
deepanwar <deepanwar@users.noreply.github.com>
2024-09-24 19:17:45 +0530

    feat(structure): add vercel.json, relocate app.js

--- app.js
+++ app.js
@@ -3,20 +3,34 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
-import indexRoutes from "./routes/index.js";
+import indexRoutes from "./src/routes/index.js";
 
 const app = express();
 
 const PORT = process.env.NODE_PORT;
 
-app.use(cors({ origin: true, credentials: true }));
+const allowedOrigins =
+  process.env.CORS_ORIGINS?.split(",").map((item) => item?.trim()) || [];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(logger("dev"));
 
 app.use("/api", indexRoutes);
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.status(200).send("SSL Checker Server is LIVE");
 });
 
 app
